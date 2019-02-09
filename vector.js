c50de8b4aae20a4e4e0f3946ad944f38b94ca232
scott <scott.m.burdick@gmail.com>
2019-02-08 23:50:39 -0500

    Begin vector class

---
+++ vector.js
@@ -0,0 +1,30 @@
+class Vector {
+  constructor(a) {
+    this.a = a;
+  }
+
+  add(a, b) {
+
+  }
+
+  subtract(a, b) {
+
+  }
+
+  multiplication(a, b) {
+
+  }
+
+  dot_product(a, b) {
+
+  }
+
+  cross_product(a, b) {
+
+  }
+
+  rotation(a, b) {
+    
+  }
+
+}

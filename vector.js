2a1b64683ef3d199568cf76b62cd911383946df1
scott <scott.m.burdick@gmail.com>
2019-02-09 00:06:20 -0500

    Add skeleton for static methods required for vectors

--- vector.js
+++ vector.js
@@ -4,27 +4,27 @@ class Vector {
     this.y = y;
   }
 
-  add(a, b) {
+  static add(a, b) {
 
   }
 
-  subtract(a, b) {
+  static subtract(a, b) {
 
   }
 
-  multiplication(a, b) {
+  static multiplication(a, b) {
 
   }
 
-  dot_product(a, b) {
+  static dot_product(a, b) {
 
   }
 
-  cross_product(a, b) {
+  static cross_product(a, b) {
 
   }
 
-  rotation(a, b) {
+  static rotation(a, b) {
 
   }
 

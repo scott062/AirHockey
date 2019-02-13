80935dfd994e738aa10de1bef0ca31a02addde6a
scott <scott.m.burdick@gmail.com>
2019-02-13 16:43:56 -0500

    Modify PVector class and add static methods - need to ammend to determine class vs instant methods

--- vector.js
+++ vector.js
@@ -1,19 +1,34 @@
-class Vector {
+class PVector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
 
   static add(a, b) {
-
+    let x = a.x + b.x;
+    let y = a.y + b.y;
+    return(x, y);
   }
 
   static subtract(a, b) {
-
+    let x = a.x - b.x;
+    let y = a.y - b.y;
+    return(x, y);
   }
 
-  static multiplication(a, b) {
+  static multiplication(a, n) {
+    x = a.x * n;
+    y = a.y * n;
+    return (a, y);
+  }
 
+  static magnitude() {
+    mag = Math.sqrt((x * x) + (y * y));
+    return mag;
+  }
+
+  normalize() {
+    x = this.x 
   }
 
   static dot_product(a, b) {
